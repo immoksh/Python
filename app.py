f38e40fc9c14de36231207e649a2ac13eef10264
dev6 <setu.m@logicaldna.com>
2018-08-06 13:15:01 +0530

    hello world program, drawing shapes & variable and data types

--- app.py
+++ app.py
@@ -14,3 +14,4 @@ print("he was " + character_age + " years old. ")
 character_name = "Mike"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
