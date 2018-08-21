ca4bd5f4d28633eef51d3dc427b09169534dcbc6
dev6 <setu.m@logicaldna.com>
2018-08-22 00:22:29 +0530

    mad libs game, basic calculator

---
+++ app.py
@@ -0,0 +1,35 @@
+# from math import *
+
+# print("Hello world")
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/   |")
+
+# my_num = -6
+# print(abs(my_num))
+# print(pow(4, 2))
+# print(sqrt(64))
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "! You are " + age)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = int(num1) + float(num2)
+# print(result)
+
+
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a Plural noun: ")
+# celebrity = input("Enter a celebrity: ")
+#
+#
+# print("Roses are " + color)
+# print(plural_noun + " are blue")
+# print("I love " + celebrity)
+
+friends = ["Parag", "Nishad", "Nilesh"]
+
