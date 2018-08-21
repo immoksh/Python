6f8471d55c3c3147310091147712e1f321bdc2f2
dev6 <setu.m@logicaldna.com>
2018-08-21 12:29:39 +0530

    lists, Lists functions, Tuples

--- app.py
+++ app.py
@@ -18,6 +18,44 @@
 # print("Setu\nModi")
 # print("Setu\"Modi")
 
-phrase = "Setu Modi"
-print(phrase.upper().isupper())
-print(len(phrase))
+# phrase = "Setu Modi"
+# print(phrase.upper().isupper())
+# print(len(phrase))
+
+
+#Lists
+# friends = ["Pari","Nilyo","Ishudi","Ghugal","Bhau"]
+# friends[4] = "Niha"
+# print(friends[-1])
+# print(friends[1])
+# print(friends[1:])
+# print(friends[2:4])
+
+
+#Lists function
+
+# lucky_numbers = [42,8,14,21,41,50]
+# friends = ["Pari","Nilyo","Nilyo","Ishudi","Ghugal","Bhau"]
+# friends.extend(lucky_numbers)
+# friends.append("Sharma")
+# friends.insert(2, "Sharma")
+# friends.remove("Ishudi")
+# friends.clear()
+# friends.pop()
+# friends.sort()
+# lucky_numbers.reverse()
+# print(lucky_numbers)
+# friends2 = friends.copy()
+# print(friends)
+# print(friends2)
+# print(friends.index("Ghugal"))
+# print(friends.count("Nilyo"))
+
+
+#Tuples
+
+# You can't change value of tuple and it's notify by round bracket (). We use tuple when data not gonna be change.
+
+# coordinates = (4, 5)
+coordinates = [(4, 5), (6, 7), (58, 76), (90, 45)]
+print(coordinates[1])
