3db108a110595bcaaaecdea75ac668cc454c52c3
dev6 <setu.m@logicaldna.com>
2018-08-21 11:31:12 +0530

    python basics

--- app.py
+++ app.py
@@ -5,13 +5,19 @@
 # print("  / |")
 # print(" /  |")
 
-character_name = "Tom"
-character_age = "60"
-is_male = True
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
+# character_name = "Tom"
+# character_age = "60"
+# is_male = True
+# print("There once was a man named " + character_name + ", ")
+# print("he was " + character_age + " years old. ")
+#
+# character_name = "Mike"
+# print("He really liked the name " + character_name + ", ")
+# print("but didn't like being " + character_age + ".")
 
-character_name = "Mike"
-print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+# print("Setu\nModi")
+# print("Setu\"Modi")
 
+phrase = "Setu Modi"
+print(phrase.upper().isupper())
+print(len(phrase))
